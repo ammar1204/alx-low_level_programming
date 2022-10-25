4b8f08479af79aecc15e4a7089513ca5d6f0ee48
ammar1204 <ammaar0406@gmail.com>
2022-10-24 20:42:18 -0700

    sggfd

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0o;
+
 
 	for (; str[i] != '\0'; i++)
 	{
 	         if ((i % 2) == 0)
-		        _putchar(str[i]);
-	         else
-
-		         continue;
+			 _putchar(str[i]);
+		 else
+			 continue;
 	}
 	_putchar('\n');
 }
