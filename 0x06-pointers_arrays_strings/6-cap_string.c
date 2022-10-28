4d35c5f402dbfea9c4e3577c874f410845370e69
ammar1204 <ammaar0406@gmail.com>
2022-10-28 12:58:26 -0700

    sfg

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words
+ * @str: strings
+ * Retur:n: pointer to string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+                while (		
