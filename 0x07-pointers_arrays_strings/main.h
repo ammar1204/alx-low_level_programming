7b28268917cc17400b237b93965390d533a6e60d
ammar1204 <ammaar0406@gmail.com>
2022-10-31 15:11:39 -0700

    sfghc

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
 
 #endif
