ccb4bf3ca94b5ce30ef350011973fd8c5e516f75
ammar1204 <ammaar0406@gmail.com>
2022-10-31 14:45:25 -0700

    sfghc

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * set_string - sets the value
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
 
 #endif
