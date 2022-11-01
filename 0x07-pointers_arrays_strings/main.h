e2518590c03f19c92ca76a545e8c4fe5fe5c8c54
ammar1204 <ammaar0406@gmail.com>
2022-10-31 21:24:48 -0700

    asff

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
+char *_strchr(char *s, char c);
 
 #endif
